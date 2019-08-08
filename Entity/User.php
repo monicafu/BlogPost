19892e360bbdd34e2231afec72d07e972b5def84
monicafu <31578677+monicafu@users.noreply.github.com>
2019-08-07 18:03:07 -0700

    added validation constrains and regex

--- Entity/User.php
+++ Entity/User.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ApiResource(
@@ -32,22 +33,34 @@ class User implements UserInterface
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
      * @Groups({"read"})
+     * @Assert\NotBlank()
+     * @Assert\Length(min=6,max=255)
      */
     private $username;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * @Assert\NotBlank()
+     * @Assert\Regex(
+     *     pattern="/(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{7,}/",
+     *     message="Password must be 7 characters long and contains at least one digit, one upper case and one lower case."
+     * )
      */
     private $password;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
      * @Groups({"read"})
+     * @Assert\NotBlank()
+     * @Assert\Length(min=5,max=255)
      */
     private $fullname;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * @Assert\NotBlank()
+     * @Assert\Email()
+     * @Assert\Length(min=6,max=255)
      */
     private $email;
 
